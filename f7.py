f70d47ebf5be28edcd3089e231840f2b25e22ea6
loczylevi <loczy2003@gmail.com>
2020-11-08 16:21:18 +0000

    #a. a = int(input('Első szám: ')) b = int(input('Második szám: ')) print(a, '*', b, '=', a * b) #b,
    a = int(input('Első szám: '))
    b = int(input('Második szám: '))
    print(a, '-', b, '=', a - b)
    #c
    a = int(input('Első szám: '))
    b = int(input('Második szám: '))
    print(a, '+', b, '=', a + b)
    
    #d
    
    a = int(input('Első szám: '))
    b = int(input('Második szám: '))
    print(a, '/', b, '=', a / b)

---
+++ f7.py
@@ -0,0 +1,22 @@
+#a.
+a = int(input('Első szám: '))
+b = int(input('Második szám: '))
+print(a, '*', b, '=', a * b)
+#b,
+
+a = int(input('Első szám: '))
+b = int(input('Második szám: '))
+print(a, '-', b, '=', a - b)
+#c
+a = int(input('Első szám: '))
+b = int(input('Második szám: '))
+print(a, '+', b, '=', a + b)
+
+#d
+
+
+a = int(input('Első szám: '))
+b = int(input('Második szám: '))
+print(a, '/', b, '=', a / b)
+
+
