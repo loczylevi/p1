3a0bedac434c68e6a00601bb136a8d5d36d61459
loczylevi <loczy2003@gmail.com>
2020-11-08 14:33:45 +0000

    print('Hello ....!') print('Nagyon jó ez a program!') print('Üdv, a program.')
    print('Hello ....! /n Nagyon jó ez a program! /n Üdv, a program /n ')

--- f2.py
+++ f2.py
@@ -2,6 +2,5 @@ print('Hello ....!')
 print('Nagyon jó ez a program!')
 print('Üdv, a program.')
 
-print('Hello ....! /n
-Nagyon jó ez a program!/n
-Üdv, a program.')
+print('Hello ....! /n Nagyon jó ez a program! /n Üdv, a program /n ')
+
